d70cd05f7fd3b5eabe152c00573b2353b0025da1
Anwesh43 <anweshthecool0@gmail.com>
2017-04-26 11:40:28 +0530

    Adding DialogView

---
+++ checkboxgroupdialog/src/main/java/com/anwesome/ui/checkboxgroupdialog/elements/DialogView.java
@@ -0,0 +1,59 @@
+package com.anwesome.ui.checkboxgroupdialog.elements;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anweshmishra on 26/04/17.
+ */
+public class DialogView extends View {
+    private int time = 0;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private List<CheckBoxElement> checkBoxElements = new ArrayList<>();
+    private ButtonElement buttonElement;
+    public DialogView(Context context,List<String> titles) {
+        super(context);
+        initCheckBoxes(titles);
+        buttonElement = new ButtonElement(new ButtonElement.ButtonClickListener() {
+            @Override
+            public void onButtonClick() {
+                setVisibility(INVISIBLE);
+            }
+        });
+    }
+    public void initCheckBoxes(List<String> titles) {
+        for(String title:titles) {
+            checkBoxElements.add(new CheckBoxElement(title));
+        }
+    }
+    public void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        int w = canvas.getWidth(),h = canvas.getHeight();
+        if(time == 0) {
+            int y = h/30,gap = h/15;
+            for(CheckBoxElement checkBoxElement:checkBoxElements) {
+                checkBoxElement.setDimension(w/10,y,4*w/5);
+                y+=(3*gap/2);
+            }
+        }
+        for(CheckBoxElement checkBoxElement:checkBoxElements) {
+            checkBoxElement.draw(canvas,paint);
+        }
+        buttonElement.draw(canvas,paint);
+        time++;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            float x = event.getX(), y = event.getY();
+            
+        }
+        return true;
+    }
+}
